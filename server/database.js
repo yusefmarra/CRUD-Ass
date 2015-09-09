5072375cd2413467a7a1566b16a26587d4c3207b
yusefmarra <yusef.marra@gmail.com>
2015-09-09 16:41:52 -0600

    heroku shit

--- server/database.js
+++ server/database.js
@@ -1,9 +1,7 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-var uriUtil = require('mongodb-uri');
 // require('dotenv').load();
 
-var mongooseURI = uriUtil.formatMongoose('mongodb://yusef:fuckyouchuck@ds051868.mongolab.com:51868/heroku_tk99p4xd');
 
 var Student = new Schema(
   {
@@ -17,5 +15,4 @@ var Student = new Schema(
 );
 
 mongoose.model('students', Student);
-mongoose.connect('mongodb://yusef:fuckyouchuck@ds051868.mongolab.com:51868/heroku_tk99p4xd');
-// ||'mongodb://'+process.env.username+':'+process.env.password+'@localhost/students');
+mongoose.connect('mongodb://'+process.env.username+':'+process.env.password+'@ds051868.mongolab.com:51868/heroku_tk99p4xd'||'mongodb://'+process.env.username+':'+process.env.password+'@localhost/students');
